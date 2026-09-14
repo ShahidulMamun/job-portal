35f2fad1b97838a3752df3e2d2f3bd9b19b97ed1
ShahidulMamun <mdshahidulcse@gmail.com>
2026-09-14 15:08:22 +0600

    refactor(admin/jobs): improve trashed job blade design

--- resources/views/admin/jobs/trash.blade.php
+++ resources/views/admin/jobs/trash.blade.php
@@ -1,53 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Admin Dashboard</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        body {
-            display: flex;
-        }
-        .sidebar {
-            width: 250px;
-            height: 100vh;
-            background: #343a40;
-            color: white;
-            padding: 20px;
-        }
-        .sidebar a {
-            color: #ccc;
-            text-decoration: none;
-            display: block;
-            margin-bottom: 10px;
-        }
-        .sidebar a:hover {
-            color: #fff;
-        }
-        .main {
-            flex-grow: 1;
-            padding: 20px;
-        }
-    </style>
-</head>
-<body>
+@extends('admin.layouts.app')
 
-    <div class="sidebar">
-        <h4>Admin Panel</h4>
-        <hr>
-        <a href="{{ route('admin.dashboard') }}">Dashboard</a>
-        <a href="{{route('admin.pending.jobs')}}">Pending Jobs</a>
-        <a href="{{route('admin.trashed.jobs')}}">Trash</a>
-        <a href="#">Users</a>
-        <a href="#">Settings</a>
-         <a href="{{route('admin.categories.list')}}">Add Category</a>
-        <a href="{{route('admin.profile.edit')}}">Profile</a>
+@section('content')
 
-        <form action="{{ route('admin.logout') }}" method="POST" class="mt-3">
-            @csrf
-            <button type="submit" class="btn btn-sm btn-danger">Logout</button>
-        </form>
-    </div>
 
     <div class="main">
           <table class="table table-bordered">
@@ -81,6 +35,4 @@
 </table>
     </div>
 @include('partials.toast')
-</body>
-</html>
-
+@endsection
